1dc12fd50c25a178053735f9900bbb8b90695ac3
Angshu Das <angshudas012@gmail.com>
2023-01-12 19:38:07 +0530

    register user completed

--- src/component/Form/Register.js
+++ src/component/Form/Register.js
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react'
 import Form from './Form';
+import useToast from '../../hooks/useToast';
+import axios from 'axios';
 
 function Register() {
     const input = [
@@ -30,11 +32,37 @@ function Register() {
         link : '/login'
     }
 
-    const register = ()=>{
+    const createToast = useToast();
+
+    const register = (event)=>{
+        event.preventDefault();
+        // console.log('here');
         const [username,email,password,cnfPassword] = input.map(elem=>{
             return elem.ref.current.value.trim();
         })
+        const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if( !emailRegex.test(email) ){
+            createToast({ text : 'email not valid' });
+            return;
+        }
+        if( cnfPassword!==password ){
+            createToast({ text : 'confirm password did not match password' });
+            return;
+        }
 
+        const toast = createToast({ text : 'registering user', type : 'promise-pending' });
+        try {
+            const res = axios.post('http://localhost:3500/user/register',{
+                username,
+                password,
+                email
+            });
+            toast.update({ text : 'user registered', type : 'promise-resolved' });
+            
+        } catch (error) {
+            toast.update({ type : 'promise-rejected', text : 'could not register user' });
+            console.log(error);
+        }
         
     }
   return (
